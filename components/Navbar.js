c5bc349baa09ae545648983c41b4ec98811ed5b6
danlakelake <108158856+danlakelake@users.noreply.github.com>
2022-09-29 17:00:43 -0500

    Initial commit

---
+++ components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import favoriteContext from "../contexts/favoriteContext";
+
+const { useContext } = React;
+
+const Navbar = () => {
+  const { favoritePokemons } = useContext(favoriteContext);
+
+  console.log(favoritePokemons);
+
+  let imgUrl =
+    "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png";
+
+  return (
+    <nav>
+      <div />
+      <div>
+        <img src={imgUrl} alt="pokeapi-logo" className="navbar-img" />
+      </div>
+      &#10084;&#65039; {favoritePokemons.length}
+      <div />
+    </nav>
+  );
+};
+
+export default Navbar;
