79477e8a31601175bb17053ad0213bade597099c
Marcos Aguilar Sandoval <marcosaguilarsandoval@Marcoss-MacBook-Air.local>
2022-09-29 18:25:50 -0500

    Changes in roots

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./styles.css";
-import Navbar from "../components/Navbar";
-import Searchbar from "../components/Searchbar";
-import Pokedex from "../components/Pokedex";
-import { getPokemonData, getPokemons, searchPokemon } from "../api";
-import { FavoriteProvider } from "../contexts/favoriteContext";
-
+import Navbar from "./components/Navbar";
+import Searchbar from "./components/Searchbar";
+import Pokedex from "./components/Pokedex";
+import { getPokemonData, getPokemons, searchPokemon } from "./api";
+import { FavoriteProvider } from "./contexts/favoriteContext";
 const { useState, useEffect } = React;
 
 const localStorageKey = "favorite_pokemon";
